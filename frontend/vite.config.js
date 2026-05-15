2d1e214317ce8573392e0918869da733a61c2ab4
tarun <tarunkumar575022@gmail.com>
2026-05-15 18:02:04 +0530

    fix vite config tailwind import

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
 
   preview: {
     host: "0.0.0.0",
-    port: process.env.PORT || 4173,
+    port: Number(process.env.PORT) || 4173,
     allowedHosts: ["employee-production-14d4.up.railway.app"],
   },
 });
