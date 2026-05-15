9bccb73a412f681683acf48d1ad9c173fd67ba42
tarun <tarunkumar575022@gmail.com>
2026-05-15 17:59:02 +0530

    fix railway allowed host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-})
+  plugins: [react()],
+
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173,
+    allowedHosts: ["employee-production-14d4.up.railway.app"],
+  },
+});
